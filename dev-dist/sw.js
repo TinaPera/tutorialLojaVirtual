76c4d0712308b0c9d2674f46715522467892da4e
TinaPera <valentina.pera2007@gmail.com>
2024-07-10 14:15:51 -0300

    preparação de tela de login

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rp0ve02p1co"
+    "revision": "0.pso3oojt8h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
