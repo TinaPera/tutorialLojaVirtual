8c184bbf422f9eea420bbf54d08e9b3e144b2b03
TinaPera <valentina.pera2007@gmail.com>
2024-07-16 14:08:37 -0300

    camada de armazenamento p/ produto, imagem uploader e categoria + criação de modal para categoria

--- src/stores/product.js
+++ src/stores/product.js
@@ -1,7 +1,7 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
-import ProductService from '@/services/product';
+import ProductService from '@/service/product';
 const productService = new ProductService();
 
 export const useProductStore = defineStore('product', () => {
@@ -15,5 +15,10 @@ export const useProductStore = defineStore('product', () => {
     products.value = await productService.getProductByCategory(category_id);
   }
 
-  return { products, getProducts, getProductsByCategory };
+  async function createProduct(product) {
+    await productService.createProduct(product);
+    getProducts();
+  }
+
+  return { products, createProduct, getProducts, getProductsByCategory };
 });
