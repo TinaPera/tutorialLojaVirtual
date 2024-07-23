8908287790a0a12f58dfa8e116e4c80fe5be1165
TinaPera <valentina.pera2007@gmail.com>
2024-07-23 14:01:12 -0300

    atualização

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u45omj86mno"
+    "revision": "0.46frcmdvf38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
